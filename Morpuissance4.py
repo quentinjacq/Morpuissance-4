4277586871507f0345173f4a38bd2531e60fa376
Quentin Jacquemot <quentin.jacq@gmail.com>
2020-03-17 13:47:24 +0100

    Plus qu a rajouter laleatoire

--- Morpuissance4.py
+++ Morpuissance4.py
@@ -17,36 +17,40 @@ class Joueur:
         self.estuneIA=estuneIA
         self.numJoueur=numJoueur
         
+    def getitem(self, coord):
+        if (coord=='pseudo'):
+            return self.pseudo
     
     def Joue(self,grille, modeJeu):
+        print("Au tour de " + str(self.pseudo)) 
         if(self.estuneIA==True):
             actionfinal = self.AlphaBetaSearch(grille, modeJeu)
             grillenv=copy.deepcopy(grille)
-            grillenv[:]=list(self.Result(grillenv, actionfinal))
-            return self.Result(grillenv, actionfinal)
+            #grillenv[:]=list(self.Result(grillenv, actionfinal, self.numJoueur))
+            print(self.numJoueur)
+            return self.Result(grillenv, actionfinal, self.numJoueur)
         
         else:
             actionJoueurReel = [[],[]]
-            print("C'est le tour du Joueur %i" %self.numJoueur) 
             listeactions = self.Action(grille, modeJeu)
             if modeJeu==1:
                 entreevalable = False
                 while(entreevalable == False):
-                    CoordX=(eval(input('Quelle est la coordonnée en X ?')))
-                    CoordY=(eval(input('Quelle est la coordonnée en Y ?')))
+                    CoordX=(eval(input('En quelle coord X voulez-vous jouer ?')))
+                    CoordY=(eval(input('En quelle coord Y voulez-vous jouer ?')))
                     if(grille[CoordX][CoordY]==0):
                         entreevalable = True
                     else:
                         print("Tu peux pas jouer la")
                 
-                grille = self.Result(grille, [CoordX,CoordY], 2)
+                grille = self.Result(grille, [CoordX,CoordY], self.numJoueur)
             else :               
                 CoordX=(eval(input('Dans quelle colonne voulez vous mettre votre pion ?')))
                 for i in range (len(listeactions)):
                     if (listeactions[i][1]==CoordX):
                         CoordY =listeactions[i][0]
                 actionJoueurReel=[CoordX,CoordY]
-                grille = self.Result(grille, actionJoueurReel, 2)
+                grille = self.Result(grille, actionJoueurReel, self.numJoueur)
         return grille
             
     
@@ -57,7 +61,7 @@ class Joueur:
         actionspossibles = self.Action(grille, modeJeu)
         for i in range(len(actionspossibles)):
             grillenv=copy.deepcopy(grille)
-            grillenv[:]=list(self.Result(grillenv, actionspossibles[i]))
+            grillenv[:]=list(self.Result(grillenv, actionspossibles[i]), self.numJoueur)
             score = self.MinValue(grillenv, modeJeu)
             if (score>scoreMax):
                 scoreMax = score
@@ -80,7 +84,7 @@ class Joueur:
             actionspossibles = self.Action(grille, modeJeu)
             for i in range(len(actionspossibles)):
                 grillenv=copy.deepcopy(grille)
-                grillenv[:]=list(self.Result(grillenv, actionspossibles[i], 2))
+                grillenv[:]=list(self.Result(grillenv, actionspossibles[i], ((self.numJoueur%2)+1)))
                 score, action = self.MaxValue(grillenv, modeJeu, a, b)
                 if (score<scoreMin):
                     scoreMin = score
@@ -100,7 +104,7 @@ class Joueur:
             actionspossibles = self.Action(grille, modeJeu)
             for i in range(len(actionspossibles)):
                 grillenv=copy.deepcopy(grille)
-                grillenv[:]=list(self.Result(grillenv, actionspossibles[i]))
+                grillenv[:]=list(self.Result(grillenv, actionspossibles[i], self.numJoueur))
                 score, action = self.MinValue(grillenv, modeJeu, a, b)
                 if (score>scoreMax):
                     scoreMax = score
@@ -132,7 +136,7 @@ class Joueur:
         return actionspossibles
 
             
-    def Result(self, grille, action, numJoueur=1):#Va appliquer l'action à la grille (action est une liste avec les deux coordonnés puis la valeur)
+    def Result(self, grille, action, numJoueur):#Va appliquer l'action à la grille (action est une liste avec les deux coordonnés puis la valeur)
         grillenv=copy.deepcopy(grille)
         grillenv[action[0]][action[1]]= numJoueur
         return grillenv
@@ -153,15 +157,15 @@ class Joueur:
         #Check si gagner par lignes
         for i in range(len(grille)):
             for j in range(len(grille[i])-nombrepourgagner+1):#On parcourt toutes les cases
-                if(grille[i][j]==1 and grille[i][j+1]==1 and grille[i][j+2]==1):#Si une case est égalse à 1, on ajoute la coord au joueur 1
+                if(grille[i][j]==self.numJoueur and grille[i][j+1]==self.numJoueur and grille[i][j+2]==self.numJoueur):#Si une case est égalse à 1, on ajoute la coord au joueur 1
                     if(modeJeu == 2):
-                        if(grille[i][j+3]==1):
+                        if(grille[i][j+3]==self.numJoueur):
                             gagnant = 1
                     else:
                         gagnant = 1
-                elif(grille[i][j]==2 and grille[i][j+1]==2 and grille[i][j+2]==2):
+                elif(grille[i][j]==((self.numJoueur%2)+1) and grille[i][j+1]==((self.numJoueur%2)+1) and grille[i][j+2]==((self.numJoueur%2)+1)):
                     if(modeJeu == 2):
-                        if(grille[i][j+3]==2):
+                        if(grille[i][j+3]==((self.numJoueur%2)+1)):
                             gagnant = 2
                     else:
                         gagnant = 2
@@ -169,15 +173,15 @@ class Joueur:
         #Check si gagner par colonne
         for i in range(len(grille)-nombrepourgagner+1):
             for j in range(len(grille[i])):#On parcourt toutes les cases
-                if(grille[i][j]==1 and grille[i+1][j]==1 and grille[i+2][j]==1):#Si une case est égalse à 1, on ajoute la coord au joueur 1
+                if(grille[i][j]==self.numJoueur and grille[i+1][j]==self.numJoueur and grille[i+2][j]==self.numJoueur):#Si une case est égalse à 1, on ajoute la coord au joueur 1
                     if(modeJeu == 2):
-                        if(grille[i+3][j]==1):
+                        if(grille[i+3][j]==self.numJoueur):
                             gagnant = 1
                     else:
                         gagnant = 1
-                elif(grille[i][j]==2 and grille[i+1][j]==2 and grille[i+2][j]==2):
+                elif(grille[i][j]==((self.numJoueur%2)+1) and grille[i+1][j]==((self.numJoueur%2)+1) and grille[i+2][j]==((self.numJoueur%2)+1)):
                     if(modeJeu == 2):
-                        if(grille[i+3][j]==2):
+                        if(grille[i+3][j]==((self.numJoueur%2)+1)):
                             gagnant = 2
                     else:
                         gagnant = 2
@@ -185,15 +189,15 @@ class Joueur:
         #Check si gagnant par diagonale descendante
         for i in range(len(grille)-nombrepourgagner+1):
             for j in range(len(grille[i])-nombrepourgagner+1):#On parcourt toutes les cases
-                if(grille[i][j]==1 and grille[i+1][j+1]==1 and grille[i+2][j+2]==1):#Si une case est égalse à 1, on ajoute la coord au joueur 1
+                if(grille[i][j]==self.numJoueur and grille[i+1][j+1]==self.numJoueur and grille[i+2][j+2]==self.numJoueur):#Si une case est égalse à 1, on ajoute la coord au joueur 1
                     if(modeJeu == 2):
-                        if(grille[i+3][j+3]==1):
+                        if(grille[i+3][j+3]==self.numJoueur):
                             gagnant = 1
                     else:
                         gagnant = 1
-                elif(grille[i][j]==2 and grille[i+1][j+1]==2 and grille[i+2][j+2]==2):
+                elif(grille[i][j]==((self.numJoueur%2)+1) and grille[i+1][j+1]==((self.numJoueur%2)+1) and grille[i+2][j+2]==((self.numJoueur%2)+1)):
                     if(modeJeu == 2):
-                        if(grille[i+3][j+3]==2):
+                        if(grille[i+3][j+3]==((self.numJoueur%2)+1)):
                             gagnant = 2
                     else:
                         gagnant = 2
@@ -201,15 +205,15 @@ class Joueur:
         #Check si gagnant par diagonale montante
         for i in range(nombrepourgagner-1,len(grille)):
             for j in range(len(grille[i])-nombrepourgagner+1):#On parcourt toutes les cases
-                if(grille[i][j]==1 and grille[i-1][j+1]==1 and grille[i-2][j+2]==1):#Si une case est égalse à 1, on ajoute la coord au joueur 1
+                if(grille[i][j]==self.numJoueur and grille[i-1][j+1]==self.numJoueur and grille[i-2][j+2]==self.numJoueur):#Si une case est égalse à 1, on ajoute la coord au joueur 1
                     if(modeJeu == 2):
-                        if(grille[i-3][j+3]==1):
+                        if(grille[i-3][j+3]==self.numJoueur):
                             gagnant = 1
                     else:
                         gagnant = 1
-                elif(grille[i][j]==2 and grille[i-1][j+1]==2 and grille[i-2][j+2]==2):
+                elif(grille[i][j]==((self.numJoueur%2)+1) and grille[i-1][j+1]==((self.numJoueur%2)+1) and grille[i-2][j+2]==((self.numJoueur%2)+1)):
                     if(modeJeu == 2):
-                        if(grille[i-3][j+3]==2):
+                        if(grille[i-3][j+3]==((self.numJoueur%2)+1)):
                             gagnant = 2
                     else:
                         gagnant = 2
@@ -231,20 +235,28 @@ def AfficherGrille(grille):#Simple méthode pour afficher esthétiquement la gri
     for i in range(len(grille)):
         print("| ",end='')
         for j in range(len(grille[i])):
-            print(str(grille[i][j])+" | ",end='')
+            if(grille[i][j] == 1):
+                print("X | ",end='')
+            elif(grille[i][j] == 2):
+                print("O | ",end='')
+            else:
+                print("  | ",end='')
         print("\n -----------")
 
 
-
 if __name__== '__main__':
-    """modeJeuvalide=False
-    while(modeJeuvalde=-False):
-        print("A quel mode voulez vous jouer ?")
-        print (" 1. Tic Tac Toe")
-        print (" 2. Connect 4")
-        modeJeu = eval(input())
-    #PAS FINI"""
-    modeJeu = 1
+    modeJeuvalide=False
+    while(modeJeuvalide==False):
+        
+        print("\nBienvue dans Morpuissance 4 !\nA quel mode voulez-vous jouer ?")
+        print (" 1. Morpion")
+        print (" 2. Puissance 4")
+        modeJeu = eval(input("Saisissez le numéro correspondant : "))
+        if (modeJeu != 1 and modeJeu != 2):
+            print("Erreur : Saisie non prise en compte")
+        else:
+            modeJeuvalide = True
+    
     
     #Initialisation des taille du jeu MORPION
     if(modeJeu==1):
@@ -261,22 +273,41 @@ if __name__== '__main__':
     #On affiche l'état de la grille
     
     Joueurs = []
-    Joueurs.append(Joueur('IA',True,1))
-    Joueurs.append(Joueur('Cyprien le naze', False, 2))
     
+    for i in range(1,3):
+        pseudo = input("Saisissez le pseudo du Joueur %i : " %i)
+        IAvalide=False
+        while(IAvalide==False):
+            print("\nEst-il un IA ?")
+            print (" 1. Oui")
+            print (" 2. Non")
+            IAint = eval(input("Saisissez le numéro correspondant : "))
+            if (IAint != 1 and IAint != 2):
+                print("Erreur : Saisie non prise en compte")
+            else:
+                IAvalide = True
+                if(IAint==1):
+                    IA = True
+                else:
+                    IA = False
+        Joueurs.append(Joueur(pseudo,IA,i))
+        
+    print(Joueurs[0].pseudo)
+    print(Joueurs[1].pseudo)
     AfficherGrille(grille)
     n=0
     gagnant = -1
-    while(J1.gagnant < 0):
-        
+    while(gagnant < 0):
+        print("--------------")
         grille = Joueurs[n].Joue(grille, modeJeu)
         AfficherGrille(grille)
         n = n+1
         n = n%2
-        gagnant = J1.TerminalTest(grille, modeJeu)
+        gagnant = Joueurs[n].TerminalTest(grille, modeJeu)
         
     if(gagnant == 0):
         print("Egalité")
     else:
-        print("%i a gagné !" %Joueurs[gagnant].pseudo)
+        print(gagnant)
+        print(str(Joueurs[gagnant-1].pseudo) + " a gagné !")
     
